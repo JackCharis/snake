0f6298156a1c08caa5b2af65505922a117966eb8
Jack Charis <alex.kulakovsky93@gmail.com>
2016-12-11 18:42:00 +0300

    little changes

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,10 +8,10 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int length, Direction direction)
+       public Snake(Point tail, int length, Direction direction)
        {
-        pList = new List<Point>();
-        for (int i = 0; i < length; i++)
+       pList = new List<Point>();
+         for (int i = 0; i < length; i++)
          {
             Point p = new Point(tail);
             p.Move( i, direction);
